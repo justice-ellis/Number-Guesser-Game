a85fbe7e3c1a2b64a2a08de4de8f06e8d4975db0
unknown <justice.ellis@amalitech.org>
2022-03-02 11:44:40 -0800

    First commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+function generateTarget() {
+
+    return Math.floor(Math.random() * Math.floor(10));
+}
+
+function compareGuesses(humanGuessInput, target, computerGuess) {
+    if (Math.abs(humanGuessInput - target) <=  Math.abs(computerGuess - target)) {
+        return true;
+    } if  (Math.abs(humanGuessInput - target) >=  Math.abs(computerGuess - target)){
+        return false;
+    }
+}
+
+function updateScore(winner) {
+    if (winner == 'human') {
+        return humanScore++
+    } if (winner == "computer") {
+        return  computerScore++
+    } else {
+
+    }
+}
+
+function advanceRound() {
+    return currentRoundNumber++
+}
+
+updateScore('human');
+console.log(humanScore); // To confirm that this value increased by 1
+updateScore('computer');
+console.log(computerScore); // To confirm that this value increased by 1
+
+
+
